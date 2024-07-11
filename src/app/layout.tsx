a5a233aeaa55ad3b8aa937e50f896e158cc8609e
Arfiano Jordhy <arfianoj@gmail.com>
2024-07-11 09:45:08 +0700

    add subsets to font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ weight: "300", style: "normal" });
+const poppins = Poppins({ weight: "300", style: "normal", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LockIt App",
